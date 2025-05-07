ada57a61886a436d09bbba6a0083f6be506ae639
ThilinaMadhusanka <140851032+ThilinaMadhusanka@users.noreply.github.com>
2025-05-07 13:03:45 -0700

    Job delete

---
+++ lib/views/main_views/sub_pages/updateJobDetails.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Updatejobdetails extends StatefulWidget {
+  const Updatejobdetails({super.key});
+
+  @override
+  State<Updatejobdetails> createState() => _UpdatejobdetailsState();
+}
+
+class _UpdatejobdetailsState extends State<Updatejobdetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Text("Update details"),
+        ],
+      ),
+    );
+  }
+}
