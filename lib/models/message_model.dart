8121892a8a50eb56da988de66fbcc8d44723de7c
YashodhaRathnayaka1 <136618724+YashodhaRathnayaka1@users.noreply.github.com>
2025-05-23 21:43:26 +0530

    Update message_model.dart

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,2 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+class Job {
+  String id;
+  String title;
+  String description;
+  String location;
+  String foods;
+  String date;
+  String workTime;
+  double salary;
+  String createdBy;
+  DateTime createdAt;
+  DateTime updatedAt;
+  bool isUpdated;
+
