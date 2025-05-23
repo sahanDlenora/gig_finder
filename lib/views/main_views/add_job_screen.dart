c17eebcf0da295a03bd079550b1aca0f941dbc80
Rakindu Athukorala <136637688+rakinduathukorala@users.noreply.github.com>
2025-05-24 00:57:05 +0530

    Update add_job_screen.dart

--- lib/views/main_views/add_job_screen.dart
+++ lib/views/main_views/add_job_screen.dart
@@ -10,34 +10,7 @@ class AddJobScreen extends StatefulWidget {
 
 class _AddJobScreenState extends State<AddJobScreen> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 15,
-              vertical: 10,
-            ),
-            child: Column(
-              children: [
-                Text(
-                  "Add Jobs",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600,
-                    fontFamily: "poppins",
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                AddJobs(),
-              ],
-            ),
-          ),
-        ),
-      ),
+
     );
   }
 }
