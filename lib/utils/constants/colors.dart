820b90747e1c3722ba49f257cff9130d7dbb5311
kawshika pathum <140903656+kawshikapathum@users.noreply.github.com>
2025-05-24 00:16:03 +0530

    design colors.dart

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 // Main app Background colors
-
+const blueColor = Color.fromRGBO(0, 149, 246, 1);
+const primaryColor = Colors.white;
+const secondaryColor = Colors.grey;
 //Main app theme colors
 const mainPurpleColor = Color(0xFFC913B9);
 const mainOrangeColor = Color(0xFFF9373F);
