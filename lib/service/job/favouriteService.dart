bee7924ae71bd21d66e4182f848eba3291a01267
sahanDlenora <sdlenora@gmail.com>
2025-05-27 20:12:07 +0530

    update displaying saved jobs

--- lib/service/job/favouriteService.dart
+++ lib/service/job/favouriteService.dart
@@ -4,24 +4,29 @@ import 'package:gig_finder/models/job_model.dart';
 
 class FavouriteService {
   final _favourites = FirebaseFirestore.instance.collection('favourites');
-  final userId = FirebaseAuth.instance.currentUser!.uid;
+
+  /// Always gets the latest logged-in user
+  String get currentUserId => FirebaseAuth.instance.currentUser!.uid;
 
   Future<void> addToFavourites(Job job) async {
-    await _favourites.doc('${userId}_${job.id}').set(job.toJson());
+    await _favourites.doc('${currentUserId}_${job.id}').set(job.toJson());
   }
 
   Future<void> removeFromFavourites(String jobId) async {
-    await _favourites.doc('${userId}_$jobId').delete();
+    await _favourites.doc('${currentUserId}_$jobId').delete();
   }
 
   Future<bool> isFavourite(String jobId) async {
-    final doc = await _favourites.doc('${userId}_$jobId').get();
+    final doc = await _favourites.doc('${currentUserId}_$jobId').get();
     return doc.exists;
   }
 
   Stream<List<Job>> getFavouriteJobs() {
-    return _favourites.where('createdBy', isNotEqualTo: null).snapshots().map(
-        (snapshot) =>
+    return _favourites
+        .where(FieldPath.documentId, isGreaterThanOrEqualTo: '${currentUserId}_')
+        .where(FieldPath.documentId, isLessThan: '${currentUserId}_\uf8ff') // Range for matching current user's docs
+        .snapshots()
+        .map((snapshot) =>
             snapshot.docs.map((e) => Job.fromJson(e.data())).toList());
   }
 }
